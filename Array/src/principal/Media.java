612a362fadd9eb0aab125474d1c428cca073d3e6
IgorWinMac <38919725+IgorWinMac@users.noreply.github.com>
2023-05-25 19:24:44 -0300

    Não sei o que foi isso

--- Array/src/principal/Media.java
+++ Array/src/principal/Media.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Media {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		Scanner entrada = new Scanner(System.in);
 		
